27c154b02fa83314f7af185e1d31cd10b1ee33ab
Emmanuel Ugwu <>
2022-09-29 23:10:18 +0100

    made changes

--- udagram-api-user/src/server.ts
+++ udagram-api-user/src/server.ts
@@ -10,7 +10,6 @@ import {V0_USER_MODELS} from './controllers/v0/model.index';
 
 
 (async () => {
-
   await sequelize.addModels(V0_USER_MODELS);
 
   console.debug("Initialize database connection...");
